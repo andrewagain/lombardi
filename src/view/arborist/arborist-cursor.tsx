4b125a5474da9481c4ec4c4674c3fef29f958b31
Andrew Farmer <ahfarmer@gmail.com>
2023-06-18 17:05:13 -0700

    meow

--- src/view/arborist/arborist-cursor.tsx
+++ src/view/arborist/arborist-cursor.tsx
@@ -1,10 +1,10 @@
 import { CursorProps } from "react-arborist"
 
-export default function ArboristCursor({ top, left, indent }: CursorProps) {
+export default function ArboristCursor({ top, left }: CursorProps) {
   return (
     <div
-      className="w-full h-0 border-t-2 border-white absolute"
-      style={{ top, left, marginLeft: indent }}
+      className="w-full h-0 border-t-2 border-white border-dotted absolute"
+      style={{ top, left }}
     ></div>
   )
 }
