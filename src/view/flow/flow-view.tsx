e034fc3966bc6e6d22a0474f8dc7fd77e86f0692
Andrew Farmer <ahfarmer@gmail.com>
2023-06-20 14:32:16 -0700

    flow-edges

--- src/view/flow/flow-view.tsx
+++ src/view/flow/flow-view.tsx
@@ -1,23 +1,29 @@
+// https://reactflow.dev/
+
 import { atom, useAtomValue } from "jotai"
 import {
   Background,
   BackgroundVariant,
   Controls,
+  Edge,
   MiniMap,
   Node,
   ReactFlow,
   XYPosition,
 } from "reactflow"
 
-import { GraphNode, GraphNodeId } from "@/graph/graph-types"
-import { graphNodesAtom } from "@/graph/state/graph-atoms"
+import { GraphEdge, GraphNode, GraphNodeId } from "@/graph/graph-types"
+import { graphEdgesAtom, graphNodesAtom } from "@/graph/state/graph-atoms"
+
+type FlowNode = Node<GraphNode>
+type FlowEdge = Edge<GraphEdge>
 
 const flowPositionMapAtom = atom(new Map<GraphNodeId, XYPosition>())
 
 const flowNodesAtom = atom((get) => {
   const nodes = get(graphNodesAtom)
   const positions = get(flowPositionMapAtom)
-  const flowNodes: Node<GraphNode>[] = nodes.map((node) => {
+  const flowNodes: FlowNode[] = nodes.map((node) => {
     return {
       id: node.id,
       position: positions.get(node.id) ?? { x: 0, y: 0 },
@@ -27,12 +33,25 @@ const flowNodesAtom = atom((get) => {
   return flowNodes
 })
 
-// https://reactflow.dev/
+const flowEdgesAtom = atom((get) => {
+  const edges = get(graphEdgesAtom)
+  const flowEdges: FlowEdge[] = edges.map((edge) => {
+    return {
+      id: edge.id,
+      source: edge.source,
+      target: edge.target,
+      data: edge,
+    }
+  })
+  return flowEdges
+})
+
 export default function FlowView() {
   const nodes = useAtomValue(flowNodesAtom)
+  const edges = useAtomValue(flowEdgesAtom)
 
   return (
-    <ReactFlow nodes={nodes}>
+    <ReactFlow nodes={nodes} edges={edges}>
       <Controls />
       <MiniMap />
       <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
