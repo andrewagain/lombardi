ce0dc4467dc8c119447164767a8e1f9ed5b336d5
Andrew Farmer <ahfarmer@gmail.com>
2023-06-23 14:20:18 -0700

    fixup

--- src/interface/layout/header.tsx
+++ src/interface/layout/header.tsx
@@ -1,10 +1,19 @@
+import { Box } from "@chakra-ui/react"
+
 import GraphSelect from "../controls/graph-select"
 
 export default function Header() {
   return (
-    <header style={{ display: "flex", justifyContent: "space-between" }}>
-      Header
+    <Box
+      display="grid"
+      gridGap={2}
+      alignItems="center"
+      padding={2}
+      gridTemplateColumns="repeat(6, max-content) 1fr repeat(4, max-content)"
+      gridAutoFlow="column"
+      as="header"
+    >
       <GraphSelect />
-    </header>
+    </Box>
   )
 }
