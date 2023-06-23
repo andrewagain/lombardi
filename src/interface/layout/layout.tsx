b6672ec280d5e8e9d37e871e2a4b8f6940b6b483
Andrew Farmer <ahfarmer@gmail.com>
2023-06-23 11:55:32 -0700

    semi-working

--- src/interface/layout/layout.tsx
+++ src/interface/layout/layout.tsx
@@ -1,15 +1,36 @@
+import { Box } from "@chakra-ui/react"
+
 import { PersistGraphEffect } from "@/graph/graph-persist.tsx"
 import AtomView from "@/view/atom/atom-view.tsx"
 import FlowView from "@/view/flow/flow-view.tsx"
 import TreeView from "@/view/tree/tree-view.tsx"
 
 import { PaneDivider } from "../controls/pane-divider"
-import style from "./app.module.css"
 import Header from "./header"
 
 export default function Layout() {
   return (
-    <main className={style.app}>
+    <Box
+      css={{
+        display: "grid",
+        gridTemplateColumns: "auto auto 1fr auto auto",
+        gridTemplateRows: "auto 1fr auto",
+        gridTemplateAreas: `
+        "h    h     h    h     h"
+        "tree l-div flow r-div atom"
+        "f    f     f    f     f"`,
+        height: "100vh",
+        width: "100%",
+        overflow: "hidden",
+
+        "> header": {
+          gridArea: "h",
+        },
+        "> footer": {
+          gridArea: "f",
+        },
+      }}
+    >
       <Header />
       <div style={{ gridArea: "tree" }}>
         <TreeView />
@@ -24,6 +45,6 @@ export default function Layout() {
       </div>
       <footer>Footer</footer>
       <PersistGraphEffect />
-    </main>
+    </Box>
   )
 }
