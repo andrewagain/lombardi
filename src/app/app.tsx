b66af975fe59bde50a8a55b47a3a6d18e46f8e17
Andrew Farmer <ahfarmer@gmail.com>
2023-06-23 10:23:40 -0700

    meh

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -4,16 +4,21 @@ import FlowView from "@/view/flow/flow-view.tsx"
 import TreeView from "@/view/tree/tree-view.tsx"
 
 import style from "./app.module.css"
+import { PaneDivider } from "./control/pane-divider.tsx"
 import Header from "./header.tsx"
 
 export default function NuonsApp() {
   return (
     <main className={style.app}>
       <Header />
-      <TreeView />
+      <div style={{ gridArea: "tree" }}>
+        <TreeView />
+      </div>
+      <PaneDivider gridArea="l-div" orientation="left" />
       <div style={{ gridArea: "flow" }}>
         <FlowView />
       </div>
+      <PaneDivider gridArea="r-div" orientation="right" />
       <div style={{ gridArea: "atom" }}>
         <AtomView />
       </div>
