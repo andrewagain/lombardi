7f06c87b76eb564a6b7a833bed7a3b482dfc462b
Andrew Farmer <ahfarmer@gmail.com>
2023-06-23 19:42:45 -0700

    wip

--- src/graph/state/tree-atoms.ts
+++ src/graph/state/tree-atoms.ts
@@ -8,6 +8,7 @@ import {
   graphEdgeMapByTargetAtom,
   graphEdgePriorityMapAtom,
   graphNodeMapAtom,
+  graphNodeVisibilityMapAtom,
 } from "./graph-atoms.ts"
 
 export interface GraphTreeNode extends GraphNode {
@@ -32,6 +33,7 @@ export const graphTreeRootNodesAtom = atom<GraphTreeNode[]>((get) => {
   const nodeMap = get(graphNodeMapAtom)
   const edgePriorityMap = get(graphEdgePriorityMapAtom)
   const outgoingEdgeMap = get(graphEdgeMapBySourceAtom)
+  const visibilityMap = get(graphNodeVisibilityMapAtom)
 
   // Create copies of all the root nodes
   const rootTreeNodes = rootNodes.map((x) => ({ ...x }))
@@ -56,7 +58,9 @@ export const graphTreeRootNodesAtom = atom<GraphTreeNode[]>((get) => {
     })
 
     if (destinationNodes.length > 0) {
-      node.children = destinationNodes.map((x) => ({ ...x }))
+      node.children = destinationNodes
+        .filter((n) => visibilityMap.get(n.id) !== false)
+        .map((x) => ({ ...x }))
       nodeStack = [...nodeStack, ...node.children]
     }
   }
