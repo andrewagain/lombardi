43cfbb698a5228e1b349bb17f84e45dff680c95f
Andrew Farmer <ahfarmer@gmail.com>
2023-06-23 19:10:20 -0700

    icon

--- src/interface/layout/toolbar/toolbar.tsx
+++ src/interface/layout/toolbar/toolbar.tsx
@@ -1,4 +1,5 @@
-import { Box } from "@chakra-ui/react"
+import { Box, IconButton } from "@chakra-ui/react"
+import { PiTreeStructureDuotone } from "react-icons/pi"
 
 import { SidePanel } from "../../interface-state"
 import GraphSelect from "./graph-select"
@@ -11,10 +12,11 @@ export default function Toolbar() {
       gridGap={2}
       alignItems="center"
       padding={2}
-      gridTemplateColumns="repeat(1, max-content) 1fr repeat(2, max-content)"
+      gridTemplateColumns="repeat(2, max-content) 1fr repeat(2, max-content)"
       gridAutoFlow="column"
       as="header"
     >
+      <PiTreeStructureDuotone style={{ marginLeft: 10, marginRight: 10 }} />
       <PanelToggle side={SidePanel.Left} />
       <Box />
       <GraphSelect />
