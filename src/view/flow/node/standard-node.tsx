9239bc427682a488431af2071c256db905740326
Andrew Farmer <ahfarmer@gmail.com>
2023-06-26 00:16:54 -0700

    meow

--- src/view/flow/node/standard-node.tsx
+++ src/view/flow/node/standard-node.tsx
@@ -1,3 +1,4 @@
+import { Box, Text } from "@chakra-ui/react"
 import { Handle, NodeProps, Position } from "reactflow"
 
 import { GraphNode } from "@/graph/graph-types"
@@ -6,10 +7,9 @@ export default function StandardNode({ data, selected }: NodeProps<GraphNode>) {
   return (
     <>
       <Handle type="target" position={Position.Top} />
-      <div>
-        <label htmlFor="text">{data.name}</label>
-        {selected ? "Selected" : "Not selected"}
-      </div>
+      <Box border={`2px solid ${selected ? "blue" : "white"}`}>
+        <Text>{data.name}</Text>
+      </Box>
       <Handle type="source" position={Position.Bottom} />
     </>
   )
